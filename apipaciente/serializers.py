903edb93df542ab8e0fb86adee866a687b501354
Juan Jose Rojas <rojasguanemejuanjose@gmail.com>
2023-05-12 12:57:22 -0500

    json ciudades, especialidades

--- apipaciente/serializers.py
+++ apipaciente/serializers.py
@@ -2,14 +2,14 @@ from rest_framework import serializers
 from .models import Paciente
 from rest_framework import serializers
 from rest_framework import serializers
-from .serializerb import CiudadSerializer
+from .models import opciones_ciudades
 
 
 
 
 class PacienteSerializer(serializers.ModelSerializer):
+    ciudad = serializers.ChoiceField(choices=opciones_ciudades)
     class Meta:
-        ciudad = CiudadSerializer()
         model = Paciente
         fields = ('idPaciente', 'cedulaPac', 'NombreCompletoPac', 'telefonoPac', 'correoPac', 'direccionPac', 'FechaNacimientoPac', 'ciudad')
 
