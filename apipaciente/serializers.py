36cd424896e6b39b9bef471933356de76f99fff2
Juan Andrés <juabo008@gmail.com>
2023-05-12 20:22:33 -0500

    errores

--- apipaciente/serializers.py
+++ apipaciente/serializers.py
@@ -2,12 +2,12 @@ from rest_framework import serializers
 from .models import Paciente
 from rest_framework import serializers
 from rest_framework import serializers
-from cities.models import City, Country
+
 
 
 class PacienteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Paciente
         fields = ('idPaciente', 'cedulaPac', 'NombreCompletoPac', 'telefonoPac', 'correoPac', 'direccionPac', 'FechaNacimientoPac', 'cuidadPac')
-        ciudadPac = serializers.ModelChoiceField(queryset=City.objects.filter(countryname='colombia'))
+
         
