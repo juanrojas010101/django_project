0e23ca393b1a0491092d6774214cb5b92fe5a933
Juan Andrés <juabo008@gmail.com>
2023-05-12 20:13:03 -0500

    Revert "render"
    This reverts commit fb3b52bd00769f0ef31135efdec26131a2b98a9d.

--- apipaciente/models.py
+++ apipaciente/models.py
@@ -12,13 +12,13 @@ opciones_ciudades = ciudad['opciones_ciudades']
 
 class Paciente(models.Model):
     idPaciente = models.IntegerField(primary_key=True)
-    NombreCompletoPac = models.CharField(max_length=80)
+    NombreCompletoPac = models.CharField(max_length=90)
     FechaNacimientoPac = models.DateField()
     cedulaPac = models.IntegerField()
     telefonoPac = models.CharField(max_length=10)
     correoPac = models.EmailField()
-    direccionPac = models.CharField(max_length=40)
-    ciudad = models.CharField(max_length=30, choices=opciones_ciudades)
+    direccionPac = models.CharField(max_length=30)
+    ciudad = models.CharField(max_length=255, choices=opciones_ciudades)
 
 
 
--- models/models.py
+++ models/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 class Medico(models.Model):
     idMedico = models.IntegerField(primary_key=True)
-    NombreCompletoMed = models.CharField(max_length=80)
-    NumeroTarjetaMed = models.CharField(max_length=50)
+    NombreCompletoMed = models.CharField(max_length=90)
+    NumeroTarjetaMed = models.CharField(max_length=30)
     cedulaMed = models.IntegerField()
     telefonoMed = models.CharField(max_length=10)
     correoMed = models.EmailField()
