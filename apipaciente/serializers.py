0b1c70b67b8e66eb28fe287acd4447ee3af59b5a
Juan Andrés <juabo008@gmail.com>
2023-05-15 21:05:58 -0500

    commit json, vistas

--- apipaciente/serializers.py
+++ apipaciente/serializers.py
@@ -2,12 +2,16 @@ from rest_framework import serializers
 from .models import Paciente
 from rest_framework import serializers
 from rest_framework import serializers
+from .models import opciones_ciudades
 
 
 
 class PacienteSerializer(serializers.ModelSerializer):
+    ciudadPac = serializers.ChoiceField(choices=opciones_ciudades)
     class Meta:
         model = Paciente
-        fields = ('idPaciente', 'cedulaPac', 'NombreCompletoPac', 'telefonoPac', 'correoPac', 'direccionPac', 'FechaNacimientoPac', 'cuidadPac')
+        fields = ('idPaciente', 'cedulaPac', 'NombreCompletoPac', 'telefonoPac', 'correoPac', 'direccionPac', 'FechaNacimientoPac', 'ciudadPac')
+
+
 
         
