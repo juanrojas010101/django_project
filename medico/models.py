e1c906865347864282a27a56c9ad7609a59ebc9e
Juan Jose Rojas <rojasguanemejuanjose@gmail.com>
2023-06-24 14:34:00 -0500

    commit campos null

--- medico/models.py
+++ medico/models.py
@@ -11,11 +11,11 @@ class MedicoM(models.Model):
     TelefonoMed = models.CharField(max_length=10)
     CiudadMed = models.CharField(max_length=90)
     Especialidad = models.CharField(max_length=50)
-    DireccionConsultorio = models.CharField(max_length=50)
-    HoraInicio = models.CharField(max_length=1000)
-    HoraFinal = models.CharField(max_length=1000)
-    FechasDisponibilidad = models.CharField(max_length=1000)
-    PerfilProfesional = models.CharField(max_length=500)
-    Activo = models.BooleanField(null=True)
+    DireccionConsultorio = models.CharField(max_length=50, null=True, blank=True)
+    HoraInicio = models.CharField(max_length=1000, null=True, blank=True)
+    HoraFinal = models.CharField(max_length=1000, null=True, blank=True)
+    FechasDisponibilidad = models.CharField(max_length=1000, null=True, blank=True)
+    PerfilProfesional = models.CharField(max_length=500, null=True, blank=True)
+    Activo = models.BooleanField(null=True, blank=True)
 
 
