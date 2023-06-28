414f3aa0a63ae2fff74470a9518a954ed3c88437
Juan Jose Rojas <rojasguanemejuanjose@gmail.com>
2023-06-28 14:50:15 -0500

    commit giud

--- apiespecialidad/models.py
+++ apiespecialidad/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 import json
+import uuid
 
 
 # Create your models here.
@@ -12,5 +13,6 @@ opciones_especialidades = especialidad['opciones_especialidades']
 class Especialidad(models.Model):
     codEspecialidad = models.BigIntegerField(primary_key=True)
     especialidad = models.CharField(max_length=35, choices=opciones_especialidades)
+    guidEspecialista = models.UUIDField(default=uuid.uuid4, editable=False)
 
 
